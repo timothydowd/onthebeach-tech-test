17d244de8b5e703928eedcf1562f4fe02f6a8ac1
timothydowd <timdowd1984@gmail.com>
2019-06-26 20:59:31 +0100

    refactored forEach dealing with building the jobSequenceArray using the reduce to help avoid mutations.  Replaced unshift and splice with spread operator and slice methods

--- orderJobs.js
+++ orderJobs.js
@@ -10,31 +10,67 @@ const orderJobs = (jobDependencies) => {
 
     console.log('formattedJobDependenciesArray: ', formattedJobDependenciesArray)
     
-    const nonPriorityJobs = []
-    const jobSequenceArray = []
-    formattedJobDependenciesArray.forEach(jobDependency => {
-        if(jobDependency.length === 1){ // if job has no dependency
-            nonPriorityJobs.push(jobDependency) //pushes to non prioritised jobs to be added at the end
-            console.log('nonPriorityJobs: ', nonPriorityJobs)
-        } else { // else if job has a dependency
+    // const nonPriorityJobs = []
+    // const jobSequenceArray = []
+
+    // formattedJobDependenciesArray.forEach(jobDependency => {
+    //     if(jobDependency.length === 1){ // if job has no dependency
+    //         nonPriorityJobs.push(jobDependency) //pushes to non prioritised jobs to be added at the end
+    //         console.log('nonPriorityJobs: ', nonPriorityJobs)
+    //     } else { // else if job has a dependency
             
+    //         const priorityJob = jobDependency[1] // allocates priority job to const
+    //         const nonPriorityJob = jobDependency[0] // and similar for non prioity job
+            
+    //         if(!jobSequenceArray.includes(priorityJob) && !jobSequenceArray.includes(nonPriorityJob)){ //if job sequence array that we are adding to 
+    //                                                                                                        // doesn't contain the priority or non priority job
+    //             jobSequenceArray.unshift(nonPriorityJob) // unshifts job pair to front of array
+    //             jobSequenceArray.unshift(priorityJob)
+
+    //         } else if(jobSequenceArray.includes(priorityJob) && !jobSequenceArray.includes(nonPriorityJob)) { // if job sequence contains the priority job only
+    //             jobSequenceArray.splice(jobSequenceArray.indexOf(priorityJob) + 1, 0, nonPriorityJob) // find the index of it and add the non priority job after it
+                
+    //         } else if(jobSequenceArray.includes(nonPriorityJob) && !jobSequenceArray.includes(priorityJob)) { // if job sequence contains the non priority job only
+    //             jobSequenceArray.splice(jobSequenceArray.indexOf(nonPriorityJob), 0, priorityJob) // find the index of it and add the priority job before it
+
+    //         } 
+    //     }
+    // })
+
+
+    const nonPriorityJobs = formattedJobDependenciesArray.reduce((nonPriorityJobs, jobDependency) => { // creates array of jobs that have no dependencies
+        return jobDependency.length === 1 ? [...nonPriorityJobs, jobDependency] : nonPriorityJobs  // if job has no dependency add to nonPriority jobs
+    },[])
+    
+
+    const jobSequenceArray = formattedJobDependenciesArray.reduce((jobSequence, jobDependency) => {
+        if(jobDependency.length === 1){
+            return jobSequence
+        } else {
             const priorityJob = jobDependency[1] // allocates priority job to const
             const nonPriorityJob = jobDependency[0] // and similar for non prioity job
-            
-            if(!jobSequenceArray.includes(priorityJob) && !jobSequenceArray.includes(nonPriorityJob)){ //if job sequence array that we are adding to 
-                                                                                                           // doesn't contain the priority or non priority job
-                jobSequenceArray.unshift(nonPriorityJob) // unshifts job pair to front of array
-                jobSequenceArray.unshift(priorityJob)
 
-            } else if(jobSequenceArray.includes(priorityJob) && !jobSequenceArray.includes(nonPriorityJob)) { // if job sequence contains the priority job only
-                jobSequenceArray.splice(jobSequenceArray.indexOf(priorityJob) + 1, 0, nonPriorityJob) // find the index of it and add the non priority job after it
+            if(!jobSequence.includes(priorityJob) && !jobSequence.includes(nonPriorityJob)){ //if job sequence array that we are adding to 
+                // doesn't contain the priority or non priority job
+                return [priorityJob, nonPriorityJob, ...jobSequence]  // adds job pair to front of array
+            } else if(jobSequence.includes(priorityJob) && !jobSequence.includes(nonPriorityJob)) { // if job sequence contains the priority job only
                 
-            } else if(jobSequenceArray.includes(nonPriorityJob) && !jobSequenceArray.includes(priorityJob)) { // if job sequence contains the non priority job only
-                jobSequenceArray.splice(jobSequenceArray.indexOf(nonPriorityJob), 0, priorityJob) // find the index of it and add the priority job before it
+                const elementsUptoIncludingPriorityJob = [...jobSequence.slice(0, jobSequence.indexOf(priorityJob) + 1)] // store jobs upto and including priority job
+                const elementsAfterPriorityJob = [...jobSequence.slice(jobSequence.indexOf(priorityJob) + 1)] // store jobs after priority job
+                return [...elementsUptoIncludingPriorityJob, nonPriorityJob, ...elementsAfterPriorityJob] // put non priority job after priority job
+
+            } else if(jobSequence.includes(nonPriorityJob) && !jobSequence.includes(priorityJob)) { // if job sequence contains the non priority job only
+                
+                const elementsUptoIncludingNonPriorityJob = [...jobSequence.slice(0, jobSequence.indexOf(nonPriorityJob))] // store jobs upto nonPriority job
+                const elementsAfterNonPriorityJob = [...jobSequence.slice(jobSequence.indexOf(nonPriorityJob))] // store jobs after and including priority job
+                return [...elementsUptoIncludingNonPriorityJob, priorityJob, ...elementsAfterNonPriorityJob] // put non priority job after priority job
 
             } 
         }
-    })
+
+    },[])
+
+
     console.log('jobSequenceArray: ', jobSequenceArray)
 
     const finalJobSequenceArray = nonPriorityJobs.reduce((finalJobSequence, nonPriorityJob) => { //takes nonpriority jobs and combines them with the rest of the job sequence if not already allocated
